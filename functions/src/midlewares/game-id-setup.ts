7059b5a6dacdc6c7adbe382efdc55a8d91316488
omenlog <omar.enrique1211@gmail.com>
2020-05-28 16:03:07 +0300

    feat(game): implement new middleware for setup game id - GH-4
    - define new middlewares module

---
+++ functions/src/midlewares/game-id-setup.ts
@@ -0,0 +1,30 @@
+import { v1 as uuidv1 } from 'uuid';
+import { TelegrafContext } from 'telegraf/typings/context';
+import { Context } from '../types/telegraf';
+
+async function gameIdSetup(context: TelegrafContext, next: any) {
+  const ctx = context as Context;
+  if (ctx.message !== undefined) {
+    const message = ctx.message.text;
+    const initResults = /\/init/g.exec(message!);
+    if (initResults !== null) {
+      ctx.state.gameId = uuidv1();
+    } else {
+      ctx.reply('Operacion desconocida');
+    }
+  } else if (ctx.updateType === 'callback_query') {
+    const message = ctx.update.callback_query!.data;
+    const generalResults = /(\S+) (\S+)/g.exec(message!);
+    if (generalResults !== null) {
+      ctx.state.gameId = generalResults[2];
+    } else {
+      ctx.reply('Operacion desconocida');
+    }
+  } else {
+    ctx.reply('Operacion desconocida');
+  }
+
+  return next();
+}
+
+export default gameIdSetup;
