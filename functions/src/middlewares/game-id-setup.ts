15f6c67f9556055c22a5a756e1986c00e0f5e12e
omenlog <omar.enrique1211@gmail.com>
2020-06-04 01:22:39 +0300

    fix: avoid unknown operation message

--- functions/src/middlewares/game-id-setup.ts
+++ functions/src/middlewares/game-id-setup.ts
@@ -3,25 +3,33 @@ import { TelegrafContext } from 'telegraf/typings/context';
 import { Context } from '../types/telegraf';
 
 async function gameIdSetup(context: TelegrafContext, next: any) {
+  console.log('LLego el mensaje');
   const ctx = context as Context;
   if (ctx.message !== undefined) {
     const message = ctx.message.text;
     const initResults = /\/start@ppt/g.exec(message!);
     if (initResults !== null) {
       ctx.state.gameId = uuidv1();
+      return next();
     } else {
-      ctx.reply('Operacion desconocida');
+      console.log('Mensaje desconocido');
+      console.log(ctx);
+      ctx.reply('');
     }
   } else if (ctx.updateType === 'callback_query') {
     const message = ctx.update.callback_query!.data;
     const generalResults = /(\S+) (\S+)/g.exec(message!);
     if (generalResults !== null) {
       ctx.state.gameId = generalResults[2];
+      return next();
     } else {
+      console.log('en el callback llego terror');
       ctx.reply('Operacion desconocida');
     }
   } else {
-    ctx.reply('Operacion desconocida');
+    console.log('Last Else');
+    console.log(ctx);
+    ctx.reply('');
   }
 
   return next();
